bc8fb609539c9cdf08f885870c97b37f78abc21c
이준구 <jungu8745@gmail.com>
2026-06-30 13:01:50 +0900

    반복 계산 기능 구현

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,43 +6,55 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
+        while (true) {
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-        int result = 0;
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
+            int result = 0;
 
-            case '-':
-                result = num1 - num2;
-                break;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
 
-            case '*':
-                result = num1 * num2;
-                break;
+                case '-':
+                    result = num1 - num2;
+                    break;
 
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                        return;
+                    }
+                    result = num1 / num2;
+                    break;
+
+                default:
+                    System.out.println("잘못된 연산자입니다.");
                     return;
-                }
-                result = num1 / num2;
-                break;
+            }
+            System.out.println("결과 : " + result);
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+            String answer = sc.next();
 
-            default:
-                System.out.println("잘못된 연산자입니다.");
-                return;
+            if (answer.equals("exit")) {
+                break;
+            }
+
         }
 
-        System.out.println("결과 : " + result);
+        sc.close();
+
     }
 }
